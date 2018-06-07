b2708859f896141296139b62041378fefec4d5e9
SWIT <dashuai1992@live.com>
2018-06-07 12:49:29 +0800

    ''

---
+++ spring.framework/src/main/java/org/spring/framework/AppMain.java
@@ -0,0 +1,10 @@
+package org.spring.framework;
+
+/**
+ * author: Swit
+ */
+public class AppMain {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ spring.framework/src/main/java/org/spring/framework/annobean/AppConfig.java
@@ -0,0 +1,10 @@
+package org.spring.framework.annobean;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * author: Swit
+ */
+@Configuration
+public class AppConfig {
+}
